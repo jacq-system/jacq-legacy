9e2b477deb2baa9c574f331e6ff3cd0fa20d6529
gunthor <gunthor@f91539ed-3dec-4b7d-98df-49f797e4bdb7>
2011-07-14 12:07:18 +0000

    herbainput_changelog.sql

--- docs/herbainput_changelog.sql
+++ docs/herbainput_changelog.sql
@@ -1,3 +1,8 @@
 ALTER TABLE `tbl_tax_synonymy`
  ADD `ref_date` DATE NULL DEFAULT NULL AFTER `acc_taxon_ID`,
  ADD `source_specimenID` INT( 11 ) NULL DEFAULT NULL AFTER `source_serviceID`
+ 
+ 
+ALTER TABLE `tbl_herbardb_groups`
+ ADD `commonnameUpdate` INT( 4 ) NOT NULL ,
+ ADD `commonnameInsert` INT( 4 ) NOT NULL
