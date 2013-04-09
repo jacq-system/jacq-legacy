fc72b214247f492d8d14fe9f31622c0e0835deab
droepert <d.roepert@bgbm.org>
2013-04-09 15:30:43 +0200

    format fix

--- input/listCommonName.php
+++ input/listCommonName.php
@@ -6,8 +6,7 @@ no_magic();
 
 
 $id = intval($_GET['ID']);
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
        "http://www.w3.org/TR/html4/transitional.dtd">
 <html>
 <head>
