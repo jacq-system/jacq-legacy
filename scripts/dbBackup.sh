87de398b4f5b0cf182bf775efb8b5aa643922862
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2014-01-13 12:54:15 +0100

    fixing archive-file listing

--- scripts/dbBackup.sh
+++ scripts/dbBackup.sh
@@ -52,8 +52,8 @@ dEcho "Creating archive"
 ARCHIVE_FILE_NAME=${DATE_PREFIX}_dbBackup.tar.bz2
 /bin/tar -C ${BACKUP_PATH} -cvjf ${ARCHIVE_PATH}/${ARCHIVE_FILE_NAME} ${BACKUP_DIR_NAME}
 
-# show list info for archive files
-/bin/ls -al ${ARCHIVE_FILE_NAME}
+# show list info for archive file
+/bin/ls -al ${ARCHIVE_PATH}/${ARCHIVE_FILE_NAME}
 
 # cleanup
 dEcho "Removing temporary backup directory"
