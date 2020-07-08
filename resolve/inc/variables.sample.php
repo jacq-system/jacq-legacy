d514f38980d22acd4eba299bb164997545dc53bf
aragon112358 <joschach@ap4net.at>
2020-07-08 12:12:10 +0200

    first version of a redirection service for uuid minter type 2

---
+++ resolve/inc/variables.sample.php
@@ -0,0 +1,13 @@
+<?php
+// Database(s) configuration
+$_CONFIG['DATABASE'] = array(
+    // JACQ-INPUT database
+    'JACQ' => array(
+        'host' => 'localhost',
+        'name' => 'jacq_input',
+        'readonly' => array(
+            'user' => '',
+            'pass' => ''
+        )
+    ),
+);
