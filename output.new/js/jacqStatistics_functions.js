117bd503c52983c7f883772c57d44ec60b32201d
aragon112358 <joschach@ap4net.at>
2020-10-06 12:11:24 +0200

    moved the jacq-statistics from legacy-living to output.new
    (json-functionality still lives in legacy-living)

---
+++ output.new/js/jacqStatistics_functions.js
@@ -0,0 +1,20 @@
+/**
+ * plot data of given institution
+ * @param {integer} institution index to institution to plot
+ */
+function plotInstitution(institution)
+{
+    $.plot('#statistics_plot', [{
+            label: plotData[institution].label,
+            data: plotData[institution].data
+        }], {
+            series: {
+                bars: {
+                    show: true,
+                    barWidth: 0.6,
+                    align: "center"
+                }
+            }
+        }
+    );
+}
