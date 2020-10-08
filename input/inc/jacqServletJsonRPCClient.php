ef5446129c24fc531db577d182d5b2ec5d32df4c
aragon112358 <joschach@ap4net.at>
2020-10-08 22:21:18 +0200

    use imgserver_Prot from tbl_img_definition instead of fixed 'http'

--- input/inc/jacqServletJsonRPCClient.php
+++ input/inc/jacqServletJsonRPCClient.php
@@ -18,54 +18,54 @@ class jacqServletJsonRPCClient extends jsonRPCClient {
      * @var PDO
      */
     private $db_input = false;
-    
+
     /**
      *
      * @var string
      */
     private $key = '';
-    
+
     /**
      *
-     * @var string 
+     * @var string
      */
     private $url = '';
-    
+
     /**
      * Construct new JSON-RPC client by fetching the required properties
      * @param string $imgserver_IP Address of image server
-     * @throws Exception 
+     * @throws Exception
      */
     public function __construct($imgserver_IP) {
         $this->db_input = clsDbAccess::Connect('INPUT');
-        
+
         $dbst = $this->db_input->query('SELECT * FROM `tbl_img_definition` WHERE `imgserver_IP` = ' . $this->db_input->quote($imgserver_IP));
         $row = $dbst->fetch();
-        
+
         if( !$row ) {
             throw new Exception( 'No valid IP' );
         }
         else if( $row['is_djatoka'] != 1 ) {
             throw new Exception( 'Not a djatoka server' );
         }
-        
+
         // Fetch required properties
         $this->key = $row['key'];
-        $this->url = 'http://' . $row['imgserver_IP'] . $row['img_service_directory'] . '/jacq-servlet/ImageServer';
-        
+        $this->url = $row['imgserver_Prot'] . '://' . $row['imgserver_IP'] . $row['img_service_directory'] . '/jacq-servlet/ImageServer';
+
         // Finally call parent constructor
         parent::__construct($this->url, false);
     }
-    
+
     /**
      * Call a JSON-RPC function, but add key as first parameter
      * @param string $method
-     * @param array $params 
+     * @param array $params
      */
     public function __call($method, $params) {
         // Always add key as first parameter
         array_unshift($params, $this->key);
-        
+
         // Finally call the method
         return parent::__call($method, $params);
     }
