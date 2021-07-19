874e55503572dad233c7f59a6104313dee50630f
aragon112358 <joschach@ap4net.at>
2021-07-19 14:58:54 +0200

    corrected problem with http/https distinction

--- input/jacqServices_ptlp.php
+++ input/jacqServices_ptlp.php
@@ -28,6 +28,7 @@ function getREST($service_url)
 {
     $curl = curl_init($service_url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
     $curl_response = curl_exec($curl);
     if ($curl_response === false) {
 //        $info = curl_getinfo($curl);
