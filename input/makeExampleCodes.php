29db11321f73453c87862b9e3c945051dea873c7
aragon112358 <joschach@ap4net.at>
2022-03-31 12:10:19 +0200

    changed creation of db-table with bar- and QR-code examples

--- input/makeExampleCodes.php
+++ input/makeExampleCodes.php
@@ -15,23 +15,31 @@ foreach ($lines as $line) {
         }
     }
 }
-$dbLink->query("TRUNCATE herbarinput.code_examples");
 
+$found = $new = 0;
 foreach ($specimenIDs as $specimenID) {
-    $row = $dbLink->query("SELECT s.specimen_ID, mc.source_id, m.source_code, s.collectionID, mc.collection, s.HerbNummer
-                           FROM tbl_specimens s
-                            LEFT JOIN tbl_management_collections mc ON mc.collectionID = s.collectionID
-                            LEFT JOIN meta m ON m.source_id = mc.source_id
-                           WHERE s.specimen_ID = $specimenID")
-                  ->fetch_assoc();
-    $dbLink->query("INSERT INTO code_examples SET
-                     specimen_ID  = "  . $row['specimen_ID']  . ",
-                     source_id    = "  . $row['source_id']    . ",
-                     source_code  = '" . $row['source_code']  . "',
-                     collectionID = "  . $row['collectionID'] . ",
-                     collection   = '" . $row['collection']   . "',
-                     HerbNummer   = '" . $row['HerbNummer']   . "',
-                     Barcode      = '" . formatUnitID($specimenID)        . "',
-                     QRCode       = '" . getStableIdentifier($specimenID) . "'");
+    $result = $dbLink->query("SELECT specimen_ID FROM code_examples WHERE specimen_ID = $specimenID");
+    if ($result->num_rows > 0) {
+        $found++;
+    } else {
+        $row = $dbLink->query("SELECT s.specimen_ID, mc.source_id, m.source_code, s.collectionID, mc.collection, s.HerbNummer
+                               FROM tbl_specimens s
+                                LEFT JOIN tbl_management_collections mc ON mc.collectionID = s.collectionID
+                                LEFT JOIN meta m ON m.source_id = mc.source_id
+                               WHERE s.specimen_ID = $specimenID")
+                      ->fetch_assoc();
+        if ($row) {
+            $new++;
+            $dbLink->query("INSERT INTO code_examples SET
+                             specimen_ID  = "  . $row['specimen_ID']  . ",
+                             source_id    = "  . $row['source_id']    . ",
+                             source_code  = '" . $row['source_code']  . "',
+                             collectionID = "  . $row['collectionID'] . ",
+                             collection   = '" . $row['collection']   . "',
+                             HerbNummer   = '" . $row['HerbNummer']   . "',
+                             Barcode      = '" . formatUnitID($specimenID)        . "',
+                             QRCode       = '" . getStableIdentifier($specimenID) . "'");
+        }
+    }
 }
-echo $dbLink->error;
+echo "<html><head></head><body><pre>" . $dbLink->error . "\n" . $found . " items found\n" . $new . " new items</pre></body></html>";
