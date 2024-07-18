9afcc46148c2c66c36afb433ce30569941cfaf99
aragon112358 <joschach@ap4net.at>
2024-07-18 09:50:06 +0200

    deleted unused directory

--- input.new/index_xajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Simple wrapper script for calling the xajax modules
- *
- * We just do that so that the base path is the same for direct calls
- * and for XAJAX calls
- */
-
-require("ajax/dispatch.php");
--- input.new/js/parameters.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require( '../inc/connect.php' );
-
-$config = array(
-	'HERBARIMAGEURL' => $_OPTIONS['HERBARIMAGEURL'],
-);
-
-echo 'var config = '.json_encode($config).';';
