91500d8bc13f17c24275b8225a9553a56d4f2ee5
aragon112358 <joschach@ap4net.at>
2024-11-12 16:28:41 +0100

    move scripts in input into a subdirectory

---
+++ input/scripts/checkPhaidraCmd.php
@@ -0,0 +1,76 @@
+#!/usr/bin/php -qC
+<?php
+require_once '../inc/variables.php';
+
+ini_set("max_execution_time","3600");
+
+/** @var mysqli $dbLink */
+$dbLink = new mysqli($_CONFIG['DATABASE']['INPUT']['host'],
+                     $_CONFIG['DATABASE']['INPUT']['readonly']['user'],
+                     $_CONFIG['DATABASE']['INPUT']['readonly']['pass'],
+                     $_CONFIG['DATABASE']['INPUT']['name']);
+if ($dbLink->connect_errno) {
+    die("Database access denied!");
+}
+$dbLink->set_charset('utf8');
+
+ob_start();
+
+$dbLink->query("INSERT INTO `herbar_pictures`.`phaidra_status` SET start = NOW()");
+$statusID = $dbLink->insert_id;
+
+$items_start = $dbLink->query("SELECT count(*) AS number FROM `herbar_pictures`.`phaidra_cache`")->fetch_assoc()['number'];
+
+$items = 0;
+$result = $dbLink->query("SELECT s.specimen_ID, s.HerbNummer, tid.HerbNummerNrDigits
+                          FROM tbl_specimens s
+                           LEFT JOIN tbl_management_collections mc ON mc.collectionID = s.collectionID
+                           LEFT JOIN tbl_img_definition tid        ON tid.source_id_fk = mc.source_id
+                          WHERE mc.source_id = 1
+                           AND (s.digital_image = 1 OR s.digital_image_obs = 1)
+                           AND s.specimen_ID NOT IN (SELECT specimenID FROM herbar_pictures.phaidra_cache)");
+while ($row = $result->fetch_array()) {
+    $phaidra = false;
+
+    // ask phaidra server if it has the desired picture.
+    $ch = curl_init("https://app05a.phaidra.org/viewer/" . sprintf("WU%0" . $row['HerbNummerNrDigits'] . ".0f", str_replace('-', '', $row['HerbNummer'])));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $curl_response = curl_exec($ch);
+    if ($curl_response) {
+        $info = curl_getinfo($ch);
+        if ($info['http_code'] == 200) {
+            $phaidra = true;
+        }
+    }
+    curl_close($ch);
+
+    if ($phaidra) {
+        if (!$dbLink->query("INSERT INTO `herbar_pictures`.`phaidra_cache` SET `specimenID` = '" . $row['specimen_ID'] . "'")) {
+            echo $dbLink->error;
+        }
+        $items++;
+    }
+}
+
+$items_end = $dbLink->query("SELECT count(*) AS number FROM `herbar_pictures`.`phaidra_cache`")->fetch_assoc()['number'];
+
+$dbLink->query("UPDATE `herbar_pictures`.`phaidra_status` SET
+                 end         = NOW(),
+                 items_start = '$items_start',
+                 items_end   = '$items_end',
+                 items_added = '$items'
+                WHERE id = $statusID");
+
+ob_end_flush();
+
+/*
+SELECT s.*
+FROM herbarinput.tbl_specimens s
+ LEFT JOIN herbarinput.tbl_management_collections mc ON mc.collectionID = s.collectionID
+ LEFT JOIN herbar_pictures.phaidra_cache pc ON pc.specimenID = s.specimen_ID
+WHERE mc.source_id = 1
+ AND s.digital_image = 1
+ AND pc.specimenID IS NULL
+
+9.8.2023: 9174 Items
+*/
---
+++ input/scripts/makeSpecimensStblidCmd.php
@@ -0,0 +1,156 @@
+#!/usr/bin/php -qC
+<?php
+require_once '../inc/variables.php';
+
+ini_set("max_execution_time","3600");
+
+if (in_array("-h", $argv) || in_array("--help", $argv)) {
+    echo $argv[0] . "                                    make stable-IDs for all new HerbNumbers\n"
+       . $argv[0] . " -h  --help                         this explanation\n"
+       . $argv[0] . "     --recheck_source_id source-id  recheck this source-ID for missing stable-IDs\n";
+    die();
+}
+
+/** @var mysqli $dbLink */
+$dbLink = new mysqli($_CONFIG['DATABASE']['INPUT']['host'],
+                     $_CONFIG['DATABASE']['INPUT']['readonly']['user'],
+                     $_CONFIG['DATABASE']['INPUT']['readonly']['pass'],
+                     $_CONFIG['DATABASE']['INPUT']['name']);
+if ($dbLink->connect_errno) {
+    die("Database access denied!");
+}
+$dbLink->set_charset('utf8');
+
+
+/**
+ * @param $sql
+ * @param bool|FALSE $debug
+ * @return mysqli_result
+ */
+function dbi_query($sql, $debug=false)
+{
+    global $_OPTIONS, $dbLink;
+
+    if($debug || $_OPTIONS['debug']==1){
+        $debug=true;
+    }
+
+    $res = $dbLink->query($sql);
+
+    if(!$res) {
+        // log the error in php error log
+        error_log("SEVERE SQL-ERROR IN SCRIPT. SQL = $sql --- Error = " . $dbLink->errno . ": " . $dbLink->error);
+        if ($debug){
+            // and show it additionally, if debug is on
+            echo $sql . "\n";
+            echo $dbLink->errno . ": " . $dbLink->error . "\n";
+        }
+    }
+
+    return $res;
+}
+
+
+require_once '../inc/stableIdentifierFunctions.php';
+
+/**
+ * check, if a source-ID is given to completely recheck for missing stable-IDs
+ */
+$key = array_search("--recheck_source_id", $argv);
+if ($key && $argc > $key + 1) {
+    $check_source_id = intval($argv[$key + 1]);
+} else {
+    $check_source_id = 0;
+}
+
+ob_start();
+
+$row_start = dbi_query("SELECT DATE(`starttime`) AS startdate 
+                        FROM statusSpecimensStblid
+                        WHERE recheck_source_id IS NULL
+                        ORDER BY id DESC LIMIT 1")
+           ->fetch_array();
+$startdate = $row_start['startdate'];
+
+dbi_query("INSERT INTO statusSpecimensStblid SET starttime = NOW()" . (($check_source_id) ? ", recheck_source_id = $check_source_id" : ''));
+$startID = $dbLink->insert_id;
+
+$count_changed = $count_new = 0;
+$count = array();
+if (!$check_source_id) {
+    // make stable identifiers for all new HerbNumbers
+    $result_specimen = dbi_query("SELECT s.`specimen_ID`, mc.`collectionID`, mc.`source_id`
+                                  FROM tbl_specimens s
+                                   JOIN tbl_management_collections mc ON s.`collectionID` = mc.`collectionID`
+                                  WHERE s.`aktualdatum` >= '$startdate'
+                                  UNION 
+                                  SELECT ss.`specimen_ID`, mc.`collectionID`, mc.`source_id`  
+                                  FROM tbl_specimens_stblid ss
+                                   JOIN tbl_specimens s ON ss.specimen_ID = s.specimen_ID 
+                                   JOIN tbl_management_collections mc ON s.`collectionID` = mc.`collectionID`
+                                  WHERE ss.`stableIdentifier` IS NULL");
+} else {
+    // make stable identifiers for all specimens of a given source-id which do not already exist
+    $result_specimen = dbi_query("SELECT s.`specimen_ID`, mc.`collectionID`, mc.`source_id`
+                                  FROM tbl_specimens s
+                                   LEFT JOIN tbl_management_collections mc ON mc.`collectionID` = s.`collectionID`
+                                   LEFT JOIN tbl_specimens_stblid ss ON ss.`specimen_ID` = s.`specimen_ID`
+                                  WHERE ss.`stableIdentifier` IS NULL
+                                   AND mc.`source_id` = $check_source_id");
+}
+while ($row_specimen = $result_specimen->fetch_array()) {
+    $stblid = makeStableIdentifier($row_specimen['source_id'], array('specimen_ID' => $row_specimen['specimen_ID']), $row_specimen['collectionID']);
+    if ($stblid) {
+        $result_test_stblId = dbi_query("SELECT id, specimen_ID FROM tbl_specimens_stblid WHERE stableIdentifier = '$stblid'");
+        if ($result_test_stblId->num_rows == 0) {
+            $result_test_spcId  = dbi_query("SELECT id FROM tbl_specimens_stblid WHERE specimen_ID = " . $row_specimen['specimen_ID']);
+            dbi_query("DELETE FROM tbl_specimens_stblid WHERE specimen_ID = {$row_specimen['specimen_ID']} AND `error` IS NOT NULL");
+            dbi_query("INSERT INTO tbl_specimens_stblid SET specimen_ID = '" . $row_specimen['specimen_ID'] . "', stableIdentifier = '$stblid'");
+            if ($result_test_spcId->num_rows > 0) {
+                $count_changed++;
+                if (isset($count[$row_specimen['source_id']]['changed'])) {
+                    $count[$row_specimen['source_id']]['changed']++;
+                } else {
+                    $count[$row_specimen['source_id']]['changed'] = 1;
+                }
+            } else {
+                $count_new++;
+                if (isset($count[$row_specimen['source_id']]['new'])) {
+                    $count[$row_specimen['source_id']]['new']++;
+                } else {
+                    $count[$row_specimen['source_id']]['new'] = 1;
+                }
+            }
+        } else {
+            $row_stblId = $result_test_stblId->fetch_assoc();
+            if ($row_stblId['specimen_ID'] != $row_specimen['specimen_ID']) {
+                $result_test_error = dbi_query("SELECT id FROM tbl_specimens_stblid WHERE specimen_ID = {$row_specimen['specimen_ID']} AND `error` IS NOT NULL");
+                if ($result_test_error->num_rows == 0) {
+                    dbi_query("INSERT INTO tbl_specimens_stblid SET 
+                                specimen_ID = '{$row_specimen['specimen_ID']}',
+                                visible     = 0,
+                                error       = 'stblId $stblid already exists ({$row_stblId['specimen_ID']})',
+                                blockedBy   = {$row_stblId['specimen_ID']}"
+                             );
+                }
+            }
+        }
+    }
+}
+$details = array();
+ksort($count);
+foreach ($count as $key => $value) {
+    $row_source = dbi_query("SELECT source_code FROM meta WHERE source_id = $key")->fetch_array();
+    $details[$key] = array('source'  => $row_source['source_code'],
+                           'new'     => (isset($value['new'])) ? $value['new'] : 0,
+                           'changed' => (isset($value['changed'])) ? $value['changed'] : 0);
+}
+dbi_query("UPDATE statusSpecimensStblid SET
+            new = $count_new,
+            changed = $count_changed,
+            details = '" . $dbLink->real_escape_string(json_encode($details)) . "',
+            stoptime = NOW(),
+            output = '" . $dbLink->real_escape_string(ob_get_contents()) . "'
+           WHERE id = $startID");
+
+ob_end_flush();
