042cff0edaf760999bc4f44430f8871236d5c582
aragon112358 <joschach@ap4net.at>
2026-03-26 14:35:47 +0100

    changed errortext in editHybrids

--- input/ajax/editHybridsServer.php
+++ input/ajax/editHybridsServer.php
@@ -18,17 +18,19 @@ function checkParents($taxonID, $parent1ID, $parent2ID)
     $row = dbi_query("SELECT * FROM tbl_tax_hybrids WHERE parent_1_ID = $parent1ID AND parent_2_ID = $parent2ID")->fetch_assoc();
     $rowMirror = dbi_query("SELECT * FROM tbl_tax_hybrids WHERE parent_2_ID = $parent1ID AND parent_1_ID = $parent2ID")->fetch_assoc();
     if (!empty($row['taxon_ID_fk']) && $row['taxon_ID_fk'] != $taxonID) {
-        $response->assign('alertbox', 'innerHTML', "<a href='editSpecies.php?sel=<{$row['taxon_ID_fk']}>' target='Species'>Hybrid already exists with ID {$row['taxon_ID_fk']}</a>");
-        $response->script('$("#alertbox").css("background-color", "OrangeRed");');
-        $response->script("$(\"[name='submitUpdate']\").css('visibility', 'hidden');");
+        $text = "<a href='editSpecies.php?sel=<{$row['taxon_ID_fk']}>' target='Species'>Hybrid formula already exists with ID {$row['taxon_ID_fk']}</a>";
+        $alert = true;
     } elseif (!empty($rowMirror['taxon_ID_fk'])) {
-        $response->assign('alertbox', 'innerHTML', "<a href='editSpecies.php?sel=<{$rowMirror['taxon_ID_fk']}>' target='Species'>mirrored Hybrid already exists with ID {$rowMirror['taxon_ID_fk']}</a>");
-        $response->script('$("#alertbox").css("background-color", "");');
-        $response->script("$(\"[name='submitUpdate']\").css('visibility', 'visible');");
+        $text = "<a href='editSpecies.php?sel=<{$rowMirror['taxon_ID_fk']}>' target='Species'>Mirrored hybrid formula already exists with ID {$rowMirror['taxon_ID_fk']}</a>";
+        $alert = false;
     } else {
-        $response->assign('alertbox', 'innerHTML', "");
-        $response->script('$("#alertbox").css("background-color", "");');
-        $response->script("$(\"[name='submitUpdate']\").css('visibility', 'visible');");
+        $text = "";
+        $alert = false;
+    }
+    $response->assign('alertbox', 'innerHTML', $text);
+    $response->script("$('#alertbox').css('background-color', '" . (($alert) ? 'OrangeRed' : '') . "');");
+    if ($_SESSION['editorControl']) {
+        $response->script("$(\"[name='submitUpdate']\").css('visibility', '" . (($alert) ? 'hidden' : 'visible') . "');");
     }
 
     return $response;
